bd6b3d3bcb9e9411987acdfd1568e32f43ec237b
Oshani Sandunika <148673873+LadyChaos000@users.noreply.github.com>
2024-02-20 18:01:08 +0530

    State Management and Cart Functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,41 +1,47 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import { FaShoppingCart, FaUser } from 'react-icons/fa'
-import { LinkContainer} from 'react-router-bootstrap'
-import logo from '../assets/styles/logo.png'
-
-
+import React from "react";
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
+import logo from "../assets/styles/logo.png";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
-        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-            <Container>
-            <LinkContainer to="/">
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
             <Navbar.Brand>
-                <img src={logo} alt='GlamourClaw' />
-                GlamourClaw
+              <img src={logo} alt="GlamourClaw" />
+              GlamourClaw
             </Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                <LinkContainer to="/cart">
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
                 <Nav.Link>
-                    <FaShoppingCart /> Cart
+                  <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/signin">
+              </LinkContainer>
+              <LinkContainer to="/signin">
                 <Nav.Link>
-                    <FaUser /> Sign In
+                  <FaUser /> Sign In
                 </Nav.Link>
-                </LinkContainer>
-                </Nav>
-            </Navbar.Collapse>
-            </Container>
-        </Navbar>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
